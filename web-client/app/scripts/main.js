64fbaa944be73847703d48d2667413385216f33f
Christopher Bartling <chris.bartling@gmail.com>
2014-01-31 00:58:54 -0600

    Initial revision.
    Signed-off-by: Christopher Bartling <chris.bartling@gmail.com>

---
+++ web-client/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global webClient, $*/
+
+
+window.webClient = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    webClient.init();
+});
