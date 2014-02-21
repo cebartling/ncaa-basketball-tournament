d565cb1cca468bb143f1330977a5470132f2a886
Christopher Bartling <chris.bartling@gmail.com>
2014-02-20 23:20:57 -0600

    Wired up the UI for the round navigator.

--- web-client/app/scripts/namespaces.js
+++ web-client/app/scripts/namespaces.js
@@ -7,13 +7,15 @@ window.BasketballTournament = {
     init: function () {
         'use strict';
 
-        var json = {
-            team1: {region_seeding: 1, team_name: "Duke", record: "(23-3)"},
-            team2: {region_seeding: 16, team_name: "Texas A&T", record: "(12-15)"}
-        };
-        var model = new BasketballTournament.Models.BracketModel(json);
-        var view = new BasketballTournament.Views.TournamentBracketView({model: model});
-        $('div.main-content').html(view.render().el);
+        var roundNavigatorView = new BasketballTournament.Views.RoundNavigatorView();
+        $('div.navigator').html(roundNavigatorView.render().el);
+//        var json = {
+//            team1: {region_seeding: 1, team_name: "Duke", record: "(23-3)"},
+//            team2: {region_seeding: 16, team_name: "Texas A&T", record: "(12-15)"}
+//        };
+//        var model = new BasketballTournament.Models.BracketModel(json);
+//        var view = new BasketballTournament.Views.TournamentBracketView({model: model});
+//        $('div.main-content').html(view.render().el);
     }
 };
 
