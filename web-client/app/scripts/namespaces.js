b815403ca45ba6494c6f290abcc761e8b105dd28
Christopher Bartling <chris.bartling@gmail.com>
2014-02-23 01:05:09 -0600

    Building up the data builder.

--- web-client/app/scripts/namespaces.js
+++ web-client/app/scripts/namespaces.js
@@ -1,4 +1,5 @@
 window.BasketballTournament = {
+    DataBuilders: {},
     Models: {},
     Collections: {},
     Views: {},
@@ -11,13 +12,6 @@ window.BasketballTournament = {
         Backbone.history.start();
         var roundNavigatorView = new BasketballTournament.Views.RoundNavigatorView();
         $('div.navigator').html(roundNavigatorView.render().el);
-//        var json = {
-//            team1: {region_seeding: 1, team_name: "Duke", record: "(23-3)"},
-//            team2: {region_seeding: 16, team_name: "Texas A&T", record: "(12-15)"}
-//        };
-//        var model = new BasketballTournament.Models.BracketModel(json);
-//        var view = new BasketballTournament.Views.TournamentBracketView({model: model});
-//        $('div.main-content').html(view.render().el);
     }
 };
 
